24666e40b7232837f30081d535c6886d9d732e49
rohit chauhan <rk9331498@gmail.com>
2026-08-22 01:54:35 +0530

    jsx

--- JavascriptInJsx/src/App.jsx
+++ JavascriptInJsx/src/App.jsx
@@ -7,8 +7,9 @@ function App(){
   return (
     <>
     <h1>pure javascript 2 + 2 = {2 + 2}</h1>
-    <h1>Hi {name}</h1>
+    <h1>Hi {name.toUpperCase()}</h1>
     <p>2 *3 {2 * 3}</p>
+    
     
     <p>JSX with curly braces use to pure javascript code write in react {}</p>
     </>
