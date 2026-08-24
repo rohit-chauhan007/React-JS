cd552140e5340a9e13a4e5376c3a8fbe88a451b4
rohit chauhan <rk9331498@gmail.com>
2026-08-24 21:28:20 +0530

    Evente in React

---
+++ Events/src/App.jsx
@@ -0,0 +1,21 @@
+//Event - Events are use to controls changes
+//onClick - button or any thinng click krne pr
+
+
+import './App.css'
+
+function App() {
+  function Show(){
+    return console.log("click")
+   
+  }
+  return (
+  <>
+  <button onClick ={Show}>Click</button>
+  
+  </>
+     
+  )
+}
+
+export default App
