ba0648527b11dfc5c1e0cfbe64096d2284f968b4
rohit chauhan <rk9331498@gmail.com>
2026-08-27 08:22:15 +0530

    Array pass In state

--- ArrayStates/src/App.jsx
+++ ArrayStates/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   
  const FruitArr = () =>{
   setFruit((prevFruit) =>{
-    return [prevFruit,"Banana"];
+    return [prevFruit,"Banana","litchi"];
   })
  }
  const Array= () => {
