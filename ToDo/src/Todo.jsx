58473473e6066b6abfb984804302d4d97217e697
rohit chauhan <rk9331498@gmail.com>
2026-08-29 16:05:54 +0530

    adding functionnality

--- ToDo/src/Todo.jsx
+++ ToDo/src/Todo.jsx
@@ -1,15 +1,21 @@
+import { useState } from "react"
 
 export default function Todo(){
+    const [todo,setTodo] = useState(["sample"]);
+    console.log(todo)
+    const AddTodo = () =>{
+        console.log("click")
+    }
     return(
         <div className="main">
           <div className="todo-container">
             <h1>TO DO</h1>
             <div className="input-container">
                 <input type="text" placeholder="Enter your Task ..."></input>
-                <button>Add</button>
+                <button onClick={AddTodo}>Add</button>
             </div>
             <div className="todo-lists">
-                <li>Eet</li>
+                
             </div>
 
           </div>
