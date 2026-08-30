b24bcdc1bc2857a133f5e541f0bf1d1bc414ed04
rohit chauhan <rk9331498@gmail.com>
2026-08-30 15:11:30 +0530

    Adding uuid

--- ToDo/src/Todo.jsx
+++ ToDo/src/Todo.jsx
@@ -1,22 +1,41 @@
 import { useState } from "react"
-
+import { v4 as uuidv4 } from 'uuid';
 export default function Todo(){
-    const [todo,setTodo] = useState(["sample"]);
-    const [newTodo,setNewTodo] = ("this is sample");
+    const [todo,setTodo] = useState([]);
+    const [newTodo,setNewtodo] = useState("");
+     
+    const AddTodo = () =>{
+      {if(!newTodo == ""){
+              //  setTodo([...todo,{items:newTodo,id:uuidv4()}]);
+              setTodo((prev)=>{
+                return [...prev,{items:newTodo,id:uuidv4()}]
+              })
+              setNewtodo("");
+ 
+      }}
+       
+       
+    }
+
+    const AddNewTodo = (event) =>{
+       setNewtodo(event.target.value);
+    }
+   
+    
     
     return(
         <div className="main">
           <div className="todo-container">
             <h1>TO DO</h1>
             <div className="input-container">
-                <input type="text" placeholder="Enter your Task ..."></input>
+                <input  type="text" value={newTodo} onKeyDown={AddNewTodo} onChange={AddNewTodo} placeholder="Enter your Task ..."></input>
                 <button onClick={AddTodo}>Add</button>
             </div>
             <div className="todo-lists">
                  <ul>
-                  {todo.map((todo)=>(
-                    <li>{todo}</li>
-                  ))}
+                    {todo.map((todo)=>(
+                      <li key={todo.id}>{todo.items}</li>
+                    ))}
                  </ul>
             </div>
           </div>
