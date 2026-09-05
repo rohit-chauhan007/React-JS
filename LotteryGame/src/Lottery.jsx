89d1c87b9a7b11ec36a2bbfe63782d1e7d00224d
rohit chauhan <rk9331498@gmail.com>
2026-09-05 09:58:18 +0530

    lottery game complete

--- LotteryGame/src/Lottery.jsx
+++ LotteryGame/src/Lottery.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 
 export default function Lottery(){
     const [randomNum,setRandomNum] = useState(0);
+    const [sum,setSum] = useState(0);
+
     const RandomNumGenrator = () =>{
-         console.log("clicked");
-       const randomNumber = Math.floor(Math.random()*1000);
+       const randomNumber = Math.floor(Math.random()*900)+100;
        setRandomNum(randomNumber);
-       console.log(randomNum);
+        const splitNum = String(randomNumber).split("");
+        const number = splitNum.map(Number);
+        console.log(number)
+        let total = 0;
+        {for (let i = 0; i < number.length; i++){
+           total = total + number[i];    
+        }}
+        setSum(total);
 
     }
     
@@ -19,6 +27,7 @@ export default function Lottery(){
                  <h1>Ticket</h1>
                  <div className="ticket-number">{randomNum}</div>
                  <button onClick={RandomNumGenrator}>Generate</button>
+                {sum == 15 ? <h3 style={{color:"white",fontSize:"1.5rem",marginTop:"1rem"}}>Congratulation you won 🥳</h3> :null}
                </div>
             </div>  
         </div>
