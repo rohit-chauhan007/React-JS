eebac5a226d90bc958e575a399a29759ab2ba9a3
rohit chauhan <rk9331498@gmail.com>
2026-09-14 08:27:20 +0530

    multiple form handle

--- Forms/src/Form.jsx
+++ Forms/src/Form.jsx
@@ -1,16 +1,29 @@
 import { useState } from "react"
 
 export default function Form(){
-    const [fullName,setFullName] = useState("");
-
-    const handleForm =(evt) =>{
-        console.log(evt.target.value)
-        setFullName(evt.target.value);
-    }
+    const [formData,setFormData] = useState({fullName:"",userName:"",Password:""});
+ 
+     const handleInputField = (evt) =>{
+       // evt.target.name//field(1)
+       // evt.target.value; value(2)
+       setFormData((currData)=>{//(3) [bracekts] = computing property fieldName nhi pta hota h
+          return {...currData,[evt.target.name] : evt.target.value}
+       })
+     }
+     
+     const defaultHandler = (event) =>{
+        event.preventDefault();
+        setFormData({fullName:"",userName:"",Password:""});
+     }
     return (
-        <form>
-        <label htmlFor="userName">FullName</label>
-        <input   onChange={handleForm} type="text" placeholder="Enter your FullName" value={fullName}/>
+        <form onSubmit={defaultHandler}>
+        <label htmlFor="FullName">FullName</label>
+        <input onChange={handleInputField}  id="FullName"  type="text" placeholder="Enter your FullName" value={formData.fullName} name="fullName" />
+ <br/><br/>
+          <label htmlFor="userName">UserName</label>
+        <input  onChange={handleInputField} id="UserName"  type="text" placeholder="Enter your UserName" value={formData.userName} name="userName"/>
+          <label htmlFor="Password">Password</label>
+        <input  onChange={handleInputField} id="UserName"  type="password" placeholder="Enter your Password" value={formData.Password} name="Password"/>
         <button >Submit</button>
         </form>
     )
